0e6efb8aecd7eb7ec55bb4f1666296a95190c14d
WangLei <wanglei0717@outlook.com>
2019-02-25 21:07:26 +0800

    Complete the method to pop the last element in an array

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -42,7 +42,7 @@ public class CollectionOperator {
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
