bf35c792244c8d5a755a959ccda47007910aaf9f
WangLei <wanglei0717@outlook.com>
2019-02-28 15:22:29 +0800

    Use lambda expression to simplify code

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -16,43 +16,34 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        List<Integer> evenList = new ArrayList<>();
+//        List<Integer> evenList = new ArrayList<>();
 //        for (Integer integer : this.array) {
 //            if (0 == integer % 2) {
 //                evenList.add(integer);
 //            }
 //        }
 //        return evenList;
-        return this.array.stream().filter(item -> item % 2 == 0).collect(Collectors.toList());
+        return this.array.stream().filter(item -> 0 == item % 2).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        List<Integer> filteredList = new ArrayList<>();
-        for (Integer integer : this.array) {
-            if (0 == integer % 3) {
-                filteredList.add(integer);
-            }
-        }
-        return filteredList;
+        return this.array.stream().filter(item -> 0 == item % 3).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        List<Integer> filteredList = new ArrayList<>();
-        for (Integer integer : firstList) {
-            if (secondList.contains(integer)) {
-                filteredList.add(integer);
-            }
-        }
-        return filteredList;
+        List<Integer> commonElements = new ArrayList<>(firstList);
+        commonElements.retainAll(secondList);
+        return commonElements;
     }
 
     public List<Integer> getDifferentElements() {
-        List<Integer> filteredList = new ArrayList<>();
-        for (Integer integer : this.array) {
-            if (!filteredList.contains(integer)) {
-                filteredList.add(integer);
-            }
-        }
-        return filteredList;
+//        List<Integer> filteredList = new ArrayList<>();
+//        for (Integer integer : this.array) {
+//            if (!filteredList.contains(integer)) {
+//                filteredList.add(integer);
+//            }
+//        }
+//        return filteredList;
+        return this.array.stream().distinct().collect(Collectors.toList());
     }
 }
