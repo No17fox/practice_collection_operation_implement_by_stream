83b9dedccb2fed809e071fef44b6d1f9d106aef0
WangLei <wanglei0717@outlook.com>
2019-03-03 17:35:26 +0800

    Simplify the method of dividing a number

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -5,6 +5,8 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class InverseReduce {
 
@@ -17,11 +19,15 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        List<Integer> list = new ArrayList<>();
-        while (number - this.random.nextInt(3) > 0) {
-            number -= this.random.nextInt(3);
-            list.add(number);
-        }
-        return list;
+//        List<Integer> list = new ArrayList<>();
+//        while (number - this.random.nextInt(3) > 0) {
+//            number -= this.random.nextInt(3);
+//            list.add(number);
+//        }
+//        return list;
+        int interval = this.random.nextInt(3);
+        return Stream.iterate(number - interval, i -> i - interval)
+                .limit(number / interval)
+                .collect(Collectors.toList());
     }
 }
